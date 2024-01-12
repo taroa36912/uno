8c2cf9e552c16a233fcb3fb41ffdd33751a5fd19
taroa36912 <ken.kulome417@gmail.com>
2024-01-12 09:21:34 +0900

    draw-card fixed.

--- player-python-mogi/demo-player.py
+++ player-python-mogi/demo-player.py
@@ -709,7 +709,7 @@ def on_next_player(data_res):
                     return
 
                 # 以後、引いたカードが場に出せるときの処理
-                play_card = res.get('draw_card')[0]
+                play_card = res.get('draw_card')
                 special = play_card.get('special')        
                 
                 if ((str(special) is not Special.WILD_SHUFFLE) or (flag)):
@@ -722,7 +722,8 @@ def on_next_player(data_res):
                         'is_play_card': False,
                         'yell_uno': len(cards + play_card) == 1, # 残り手札数を考慮してUNOコールを宣言する
                     }
-                
+                    
+                play_card = res.get('draw_card')[0]               
                 if play_card.get('special') == Special.WILD or play_card.get('special') == Special.WILD_DRAW_4:
                     color = select_change_color(cards)
                     data['color_of_wild'] = color
